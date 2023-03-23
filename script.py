6047339407a22b9073ae46c23a15135a767bdb12
Micin <46579867+micingans@users.noreply.github.com>
2023-03-23 20:19:54 +0700

    change code to a minimum line
    pusing ngentod

--- script.py
+++ script.py
@@ -38,7 +38,6 @@ sering_muncul = angka.most_common(1)[0][0]
 kecil = min(sort)
 besar = max(sort)
 
-# print(besar)
 os.system('clear')
 print(f"\nData = {data}\n")
 print(f"Data yang sudah disusun = {sort}\n")
@@ -46,51 +45,14 @@ print(f"Jumlah data = {banyak(data)}\n")
 print(f"Rentang data = {jarak(sort)}\n")
 print(f"Panjang Kelas data = {panjang_kelas(data)}\n")
 print(f"Kelas data = {kelas(sort)}\n")
-print(f"Interval data {kecil}-{kecil + panjang_kelas(sort)-1}")
-for x in range(kecil, kecil + panjang_kelas(sort)):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
 
-print(f"Interval data {kecil + panjang_kelas(sort)}-{(kecil + panjang_kelas(sort)) + panjang_kelas(sort)-1}")
-for x in range(kecil + panjang_kelas(sort), (kecil + panjang_kelas(sort)) + panjang_kelas(sort)):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
-
-print(f"Interval data {(kecil + panjang_kelas(sort)) + panjang_kelas(sort)}-{((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort)-1}")
-for x in range((kecil + panjang_kelas(sort)) + panjang_kelas(sort), ((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort)):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
-
-print(f"Interval data {(kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort)}-{((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort)-1}")
-for x in range((kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort), ((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort)):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
-
-print(f"Interval data {(kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)}-{((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)-1}")
-for x in range((kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort), ((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
-
-print(f"Interval data {(kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)}-{((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)-1}")
-for x in range((kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort), ((((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)))):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
-
-print(f"Interval data {(kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)}-{((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort)-1}")
-for x in range((kecil + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort), (((kecil + panjang_kelas(sort)) + panjang_kelas(sort)) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort) + panjang_kelas(sort))):
-	for a in str(sort.count(x)):
-		banyak_data = a
-	print(f"Interval data {x} -> {banyak_data}")
-print("")
+awal = 1
+for x in range(1, (kelas(sort) + 2)):
+	for y in range(0, x+1, 1):
+		print(f"Interval data {kecil + (panjang_kelas(sort) * y + awal - 1)}-{kecil + (panjang_kelas(sort) * x) - 1}")
+		for z in range(kecil + (panjang_kelas(sort) * y + awal - 1), (kecil + (panjang_kelas(sort) * x))):
+			for a in str(sort.count(z)):
+				print(f"Interval data {z} -> {a}")
+				awal += 1
+		print("")
+		break;
